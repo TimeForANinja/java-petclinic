1564e64dc1f67441f6b0035f76fc0e5ec1c9bbf6
TimeForANinja <t.kutscha@yahoo.de>
2023-01-09 22:27:26 +0100

    try some diff cypress config options

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // experimentalStudio: true,
+    baseUrl: 'http://localhost:8080',
+    experimentalStudio: true,
   },
 });
