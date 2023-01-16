b784cf5fabd41871ea7af6ef00d3d2e815058e09
Fabian <45687944+Fabian-Anna@users.noreply.github.com>
2023-01-16 18:48:34 +0100

    copied correct Cypress testing file

--- testing/cypress/e2e/user-story-1.cy.js
+++ testing/cypress/e2e/user-story-1.cy.js
@@ -2,7 +2,7 @@
 
 describe('Delete an Owner', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit('localhost:8080');
     // navigate to owners list
     cy.get(':nth-child(2) > .dropdown-toggle').click();
     cy.get('.open > .dropdown-menu > :nth-child(1) > a').click();
@@ -13,7 +13,7 @@ describe('Delete an Owner', () => {
     cy.get(':nth-child(1) > .ownerFullName > a').click();
     cy.contains('Edit Owner').click();
 
-    cy.contains('Delete Owner');
+    cy.contains('Delete Owner').should('exist');
   })
 
   it('Check if security check message exists', () => {
@@ -22,38 +22,43 @@ describe('Delete an Owner', () => {
     cy.contains('Edit Owner').click();
 
     cy.contains('Delete Owner').click();
-    cy.contains('Are you sure you want to delete this owner and all his associated pets and visits?');
-    cy.contains('Submit Deletion');
-    cy.contains('Cancel');
+    
+    // Accessing the pop-up
+    cy.on('window:confirm', (txt) => {
+      expect(txt).to.contains('Are you sure you want to delete this owner and all his associated pets and visits?');
+    });
+
+    // Needed since cypress will press ok otherwise and delete the owner
+    cy.on('window:confirm', () => false);
   })
 
   it('Cancel deletion of an owner', () => {
+    if (!cy.contains('Paul Pausten')) {
+      cy.addNewOwner('Paul', 'Pausten', '131 Paul-Klee-Strasse', 'Bremen', '98427123');
+    }
+    
     // press the first entry in the list
-    cy.get(':nth-child(1) > .ownerFullName > a').click();
-
-    // Save Owner Details for check query later on
-    var fullName = cy.get('.ownerFullName');
-    var address = cy.get(':nth-child(2) > td');
-    var city = cy.get(':nth-child(3) > td');
-    var telephone = cy.get(':nth-child(4) > td');
+    cy.contains('Paul Pausten').click();
 
     cy.contains('Edit Owner').click();
-    var editURL = cy.url();
 
     cy.contains('Delete Owner').click();
-    cy.contains('Cancel').click();
+    // Accessing the pop-up
+    cy.on('window:confirm', (txt) => {
+      expect(txt).to.contains('Are you sure you want to delete this owner and all his associated pets and visits?');
+    });
 
-    // User is still on edit page
-    cy.url().should('equal', editURL);
+    // Needed since cypress will press ok otherwise and delete the owner
+    cy.on('window:confirm', () => false);
 
+    // User is redirected to show details page
+    cy.url().should('contain', 'petclinic/owners/' );
+
+    // Navigate back to list of all owners
     cy.contains('Back').click();
-    cy.contains('Back').click();
 
-    // Not deleted owner does still exist
-    cy.contains(fullName);
-    cy.contains(address);
-    cy.contains(city);
-    cy.contains(telephone);
+    // Check if not deleted owner does still exist
+    cy.contains('Paul Pausten').should('exist');
   })
 
   /* This testcase suffices since the requirement only specified to delete an owner.
@@ -61,34 +66,21 @@ describe('Delete an Owner', () => {
   standard 'deleteOwner' API function here - we don't need to test if the pets and/or
   visits are correctly deleted as well*/
   it('Delete a new Owner with no pets or visits', () => {
-    addNewOwner();
+    cy.addNewOwner('Bob', 'Tester', '12 Teststrasse', 'Testhausen', '947120397');
     cy.contains('Bob Tester').click();
     cy.contains('Edit Owner').click();
 
     cy.contains('Delete Owner').click();
     
-    cy.contains('Are you sure you want to delete this owner and all his associated pets and visits?');
-    
-    cy.contains('Submit Deletion').click();
-
-    cy.contains('Success');
+    // no further action needed since cypress confirms confirm-popups by default
 
     // User gets thrown back to landing page
-    cy.url().should('include', '/petclinic/welcome');
+    cy.url().should('equal', 'http://localhost:8080/petclinic/owners');
 
-    // Check if owner is not shown in owner search list
+    // Check if owner is not shown in owner search list "he is deleted"
     // navigate to owners list
     cy.get(':nth-child(2) > .dropdown-toggle').click();
     cy.get('.open > .dropdown-menu > :nth-child(1) > a').click();
+    cy.contains('Bob Tester').should('not.exist');
   })
 })
-
-function addNewOwner() {
-  cy.get('#ownersTable > div > .btn').click();
-  cy.get('#firstName').type('Bob');
-  cy.get('#lastName').type('Tester');
-  cy.get('#address').type('Teststrasse 13');
-  cy.get('#city').type('Testhausen');
-  cy.get('#telephone').type('123456789');
-  cy.get('[type="submit"]').click();
-}
