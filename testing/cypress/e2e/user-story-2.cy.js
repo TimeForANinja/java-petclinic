f8cb76df069d6a4838af673bc52f99f3c07dec43
Fabian <45687944+Fabian-Anna@users.noreply.github.com>
2023-01-21 15:56:43 +0100

    Create user-story-2.cy.js

---
+++ testing/cypress/e2e/user-story-2.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe('Show all visits for a vet', () => {
+
+    it('Show all visits for a vet', () => {
+        cy.visit('localhost:8080');
+        cy.contains('Veterinarians').click();
+        cy.contains('All').click();
+
+        // Cypress automatically uses the first found entry in the given list
+        cy.contains('Show Visits').click();
+
+        // TODO Somehow check if max of 10 entries are shown
+    })
+
+})
