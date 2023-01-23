55e736612dd6a5b047dc8ee572f1dd410b0b7aaa
Fabian <45687944+Fabian-Anna@users.noreply.github.com>
2023-01-23 22:41:05 +0100

    Update user-story-2.cy.js

--- testing/cypress/e2e/user-story-2.cy.js
+++ testing/cypress/e2e/user-story-2.cy.js
@@ -8,9 +8,11 @@ describe('Show all visits for a vet', () => {
         cy.contains('All').click();
 
         // Cypress automatically uses the first found entry in the given list
-        cy.contains('Show Visits').click();
+        cy.log(cy.get('tbody').children());
+        // cy.contains('Show Visits').click();
 
         // TODO Somehow check if max of 10 entries are shown
+
     })
 
 })
