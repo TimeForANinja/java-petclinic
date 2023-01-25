34c58b4b561bf60cf592d6164e24a6e29b68b5b8
Fabian <45687944+Fabian-Anna@users.noreply.github.com>
2023-01-25 14:07:06 +0100

    Update user-story-2.cy.js

--- testing/cypress/e2e/user-story-2.cy.js
+++ testing/cypress/e2e/user-story-2.cy.js
@@ -8,7 +8,7 @@ describe('Show all visits for a vet', () => {
         cy.contains('All').click();
 
         // Cypress automatically uses the first found entry in the given list
-        cy.log(cy.get('tbody').children());
+        cy.get('tbody').children();
         // cy.contains('Show Visits').click();
 
         // TODO Somehow check if max of 10 entries are shown
