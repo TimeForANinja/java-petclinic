85accaa09ef1cf119328038a4e8da7e7a40084e5
Fabian <45687944+Fabian-Anna@users.noreply.github.com>
2023-01-29 00:14:17 +0100

    Added the @Profile("spring-data-jpa") to vetRepository
    Still can't delete a vet with its visits tho :(

--- spring-petclinic-rest/src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataVetRepositoryImpl.java
+++ spring-petclinic-rest/src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataVetRepositoryImpl.java
@@ -1,10 +1,13 @@
 package org.springframework.samples.petclinic.repository.springdatajpa;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.model.Visit;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@Profile("spring-data-jpa")
 public class SpringDataVetRepositoryImpl implements VetRepositoryOverride {
     @PersistenceContext
     private EntityManager em;
@@ -12,6 +15,7 @@ public class SpringDataVetRepositoryImpl implements VetRepositoryOverride {
     @Override
     public void delete(Vet vet) {
         Integer vetId = vet.getId();
+
         this.em.createQuery("DELETE FROM Visit visit WHERE vet_id=" + vet.getId()).executeUpdate();
         this.em.createQuery("DELETE FROM Vet vet WHERE id=" + vet.getId()).executeUpdate();
         if (em.contains(vet)) {
